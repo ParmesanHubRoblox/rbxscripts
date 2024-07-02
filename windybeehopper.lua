d2686b11a463606a841aa2ddf0789448f3632e30
ParmesanHubRoblox <rbxparmesanhub@gmail.com>
2024-07-02 16:16:07 +0300

    Create windybeehopper.lua

---
+++ windybeehopper.lua
@@ -0,0 +1,75 @@
+repeat task.wait() until game:IsLoaded()
+
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+local WebhookURL = "https://discord.com/api/webhooks/1257659675894218793/17LmaE04p8M7FRBFoelKEIBCFtYOqxO_ZUVK0hSvPMJryVtwlezK7K-ZNZoeIkjHTu1Q"
+local Time = os.date('!*t', os.time());
+
+local function GetWebhookInfo()
+	if not game.Workspace:WaitForChild("NPCBees"):FindFirstChild("Windy") then return nil end
+	
+	for _, Monster in pairs(game.Workspace.Monsters:GetChildren()) do
+		if string.find(Monster.Name, "Windy Bee") then
+			local BeeData = {
+				Level = tostring(Monster:WaitForChild("Level").Value),
+				TimeLeft = Monster:WaitForChild("Head"):WaitForChild("GuiAttachment"):WaitForChild("MonsterGui"):WaitForChild("BarRow"):WaitForChild("Bar"):WaitForChild("NumberLabel").Text
+			}
+
+			return 
+				"Fighting: **true** \n"..
+				"Bee Level: ".."**"..BeeData.Level.."** \n"..
+				"Time Left: ".."**"..BeeData.TimeLeft.."** \n"
+		else
+			return "Fighting: **false**"
+		end
+	end
+end
+
+local function SendWebhook()
+	(http_request) {
+		Url = WebhookURL;
+		Method = 'POST';
+		Headers = {['Content-Type'] = 'application/json'};
+		Body = HttpService:JSONEncode({
+			embeds = {
+				{
+					title = "Windy Bee has been found!";
+					color = "5814783";
+					description = GetWebhookInfo(),
+					timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
+				} 
+			} 
+		});
+	};
+end
+
+local function ServerHop()
+	local Api = "https://games.roblox.com/v1/games/"
+
+	local PlaceId = game.PlaceId
+	local Servers = Api..PlaceId.."/servers/Public?sortOrder=Asc&limit=100"
+	
+	function ListServers(cursor)
+		local Raw = game:HttpGet(Servers .. ((cursor and "&cursor="..cursor) or ""))
+		return HttpService:JSONDecode(Raw)
+	end
+
+	local Server, Next
+	
+	repeat
+		local Servers = ListServers(Next)
+		Server = Servers.data[1]
+		Next = Servers.nextPageCursor
+	until Server
+
+	TeleportService:TeleportToPlaceInstance(PlaceId, Server.id, Players.LocalPlayer)
+end
+
+if GetWebhookInfo() ~= nil then
+	SendWebhook()
+	while game.Workspace.NPCBees:FindFirstChild("Windy") do task.wait() end
+end
+
+ServerHop()
